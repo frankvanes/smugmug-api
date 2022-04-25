d7f79063dc4f9dbed7707415da0fe304c2036b61
gitlab@frankvanes.org <frank@frankvanes.org>
2022-04-25 23:43:21 +0200

    Removed secrets

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 //const SmugMug = require('./dist/index.js').SmugMug;
 const sm = require('./dist/index.js');
 const SmugMug = sm.SmugMug;
-const smugmug = new SmugMug('2Wz5GDbPxJcnv3tZ5vCZFV9BSkHwmmNs','STPdJqrpVgChbmzbK2f87bcWBvD6MQSZNwpwSRL5RD7wgsHXBQmjCpgdzC8zvpC6','fj3vXM2BcRbwCsG6mKxpkFmmSQxLqGzs','ccX2785pz2T9QwFfJf9ztWsWqj5Gpz7wJ4gCwCkc56863qCWcdLHs8sxG3KgqSgL');
+const smugmug = new SmugMug();
 
 //SmugMug.request("user/frankvanes!profile?_verbosity=3").then((res) => {
 //  
